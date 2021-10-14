c51d2c4def655f6777573c7329ff6bcd0ab6cf6a
konkvi <konkvistador@yandex.ru>
2021-10-14 21:47:43 +0300

    Fix

--- src/main/java/facade/BinOps.java
+++ src/main/java/facade/BinOps.java
@@ -8,17 +8,17 @@ public class BinOps {
     public String sum(String a, String b) {
     //Переведите числа из двоичной записи в строках в int,
         // сложите и переведите обратно в двоичную запись
-        int A = Integer.parseInt(a, 2);
-        int B = Integer.parseInt(b, 2);
-        return Integer.toBinaryString(A + B);
+        int x = Integer.parseInt(a, 2);
+        int y = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(x + y);
     }
 
     public String mult(String a, String b) {
     // Переведите числа из двоичной записи в строках в int,
         // перемножьте и переведите обратно в двоичную запись
-        int A = Integer.parseInt(a, 2);
-        int B = Integer.parseInt(b, 2);
-        return Integer.toBinaryString(A * B);
+        int x = Integer.parseInt(a, 2);
+        int y = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(x * y);
     }
 
 }
